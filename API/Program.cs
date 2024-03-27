da76409102b1fdfcb5b0c15112add05345893275
Zintle Komazi 2019098256 <2019098256@ufs4life.ac.za>
2024-03-27 07:11:20 +0200

    End of section 7

--- API/Program.cs
+++ API/Program.cs
@@ -1,13 +1,6 @@
 //2019098256 ZBE Komazi
-using System.Text;
-using API.Data;
 using API.Extensions;
-using API.Interfaces;
-using API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-
+using API.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container:
@@ -17,6 +10,7 @@ builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
 // Configure the HTTP request pipeline:
+app.UseMiddleware<ExceptionMiddleware>();
    
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
@@ -26,58 +20,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- //app.UseHttpsRedirection();
-    //app.UseAuthorization();
