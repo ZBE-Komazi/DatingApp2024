a461efe0f7ac4a20024fc1eeeb6ee50347b74c18
Zintle Komazi 2019098256 <2019098256@ufs4life.ac.za>
2024-05-22 20:44:19 +0200

    "Updated Program.cs and fly.toml files with minor changes to connection string and app configuration."

--- API/Program.cs
+++ API/Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
 
 
+
 var connString="";
 if (builder.Environment.IsDevelopment()) 
     connString = builder.Configuration.GetConnectionString("DefaultConnection");
@@ -34,7 +35,7 @@ else
         var pgPass = pgUserPass.Split(":")[1];
         var pgHost = pgHostPort.Split(":")[0];
         var pgPort = pgHostPort.Split(":")[1];
-	    var updatedHost = pgHost.Replace("flycast", "internal");
+	var updatedHost = pgHost.Replace("flycast", "internal");
 
         connString = $"Server={updatedHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};";
 }
@@ -42,7 +43,6 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseNpgsql(connString);
 });
-
 
 var app = builder.Build();
 // Configure the HTTP request pipeline:
